60911e5aec61fc14592d5b33c5265ec6e74147c5
Kálmán „KAMI” Szalai <kami911gmail.com>
2026-08-31 16:14:12 +0200

    chore: Bump version to 0.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kami911.wifienabler"
         minSdk = 28
         targetSdk = 31
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
